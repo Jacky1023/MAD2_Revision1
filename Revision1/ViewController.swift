d9bcdbea37fdb4b886cf2939a7c22153a6a95280
Wong Kah Wo <s10189113@connect.np.edu.sg>
2019-12-03 13:28:53 +0800

    can link to content storyboard

--- Revision1/ViewController.swift
+++ Revision1/ViewController.swift
@@ -10,7 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
     
+    @IBAction func Loginbtn(_ sender: Any) {
+        if true{
+                  
+                  let storyboard = UIStoryboard(name: "Content", bundle: nil)
+                  let vc = storyboard.instantiateViewController(withIdentifier: "Content") as UIViewController
+                  vc.modalPresentationStyle = .fullScreen // try without fullscreen
+                  present(vc,animated: true,completion: nil)
+              }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
